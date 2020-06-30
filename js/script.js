0b9f86fe3e529763ae18709656704351aacbd1a8
Samar <cyparis.samura@yandex.ru>
2020-06-30 15:44:02 +0300

    Refactoring

--- js/script.js
+++ js/script.js
@@ -1,78 +1,64 @@
-	//Слайдер
-$(document).ready(function(){
-	$('.slider').owlCarousel({
-		items: 1,
-		loop: true,
-		nav: false,
-		autoplay: false,
-		center: true,
-		mouseDrag: true,
-		touchDrag: true,
-		dots: true
-	});
-
-	// Inputmask for phone number in contact
-	$('.phone').inputmask({"mask": "+7(499) 999-99-99"});
-}); 
-
-//Pop-up window for form
-function sweetAlertClick() {
-	var phone = $('.form__input_phone').val();
-	if(phone.length === 17) {
-		swal(
-				'Сообщение отправлено',
-				'',
-				'success'
-			);
-		form.submit();
-	} else {
-		swal(
-				'Заполните все поля',
-				'',
-				'error'
-			);
-	}
+/*   F2 Form show alert   */
+// 
+  function validate() {
+      var a = document.forms["form"]["phone"].value;
+        if (a >= 7) {
+          alert("Сообщение отправлено.");
+          return true;
+        } else {
+          alert("Некорректный номер телефона.");
+          return false;
+        }
 }
 
-//Pop-up window for a calculator with a form
-function sweetAlertClickCalculator() {
-	var size = $('.calculator__input-size').val();
-	var selectItem = $('input[name=selection-item]:checked').length;
-	var selectOption = $('.selection-criterion__list option:selected').length;
-	var phone = $('.calculator__field-phone').val();
-	if(selectItem != 0 && selectOption != 0 && size.length > 1 && phone.length === 17){
-		swal(
-				'Сообщение отправлено',
-				'',
-				'success'
-			);
-			form.submit();
-	} else {
-		swal(
-				'Заполните все поля',
-				'',
-				'error'
-			);
-	}
+/*   F3 Slider   */
+// 
+var slideIndex = 1;
+showSlides(slideIndex);
+
+// Set current slide 
+function currentSlide(n) {
+    showSlides(slideIndex = n);
 }
 
-//Uploading content on the windows
+// Main slider function 
+function showSlides(n) {
+    var i;
+    var slides = document.getElementsByClassName("slider__item");
+    var dots = document.getElementsByClassName("slider-dots__item");
+    if (n > slides.length) {
+      slideIndex = 1
+    }
+    if (n < 1) {
+        slideIndex = slides.length
+    }
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex - 1].style.display = "block";
+    dots[slideIndex - 1].className += " active";
+}
+
+/*   F5 Uploading content on the calculator   */
 function changeBox(winLink) {
-	var windowLink = document.querySelectorAll('.switching-windows__headline');
-	var windowImg = document.querySelectorAll('.calculator__img');
-	var windowTitle = document.querySelectorAll('.selection-criterion__headline');
+    var windowLink = document.querySelectorAll(".windows__link");
+    var windowImg = document.querySelectorAll(".img-wrapper__img");
+    var windowTitle = document.querySelectorAll(".parameters__headline");
+    for (var i = 0; i < windowLink.length; i++) {
+        if (windowLink[i].id == winLink) {
 
-	for (var i = 0; i < windowLink.length; i++) {
-		if (windowLink[i].id == winLink) {
-			windowLink[i].style.textDecoration = 'underline';
-			windowLink[i].style.color = '#0099de';
-			windowImg[i].style.display = 'block';
-			windowTitle[i].style.display = 'block';
-		} else {
-			windowLink[i].style.textDecoration = 'none';
-			windowLink[i].style.color = '#000';
-			windowImg[i].style.display = 'none';
-			windowTitle[i].style.display = 'none';
-		}
-	}
+            windowLink[i].style.textDecoration = "underline";
+            windowLink[i].style.color = "#0099de";
+            windowImg[i].style.display = "inline-block";
+            windowTitle[i].style.display = "inline-block";
+        } else {
+            windowLink[i].style.textDecoration = "none";
+            windowLink[i].style.color = "#000";
+            windowImg[i].style.display = "none";
+            windowTitle[i].style.display = "none";
+        }
+    }
 }
